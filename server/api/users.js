1c90ad672986e1ee8a09aad7d326581690df9ddd
youssef <youssef.sadek910@gmail.com>
2019-10-09 03:42:03 +0100

    Add Update Pic Api and query

--- server/api/users.js
+++ server/api/users.js
@@ -6,28 +6,40 @@ const usersDb = require("../services/database/users");
  * The route here will be: /users/ (remember the prefix users is defined in api/index.js)
  */
 router.get("/", (req, res) => {
-	usersDb
-		.getAllUsers()
-		.then(data => {
-			res.send(data);
-		})
-		.catch(err => {
-			console.error(err);
-			res.send(500);
-		});
+  usersDb
+    .getAllUsers()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(500);
+    });
 });
 
 router.get("/:userId", (req, res) => {
-	let id = req.params.userId;
-	usersDb
-		.getUserById(id)
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			console.error(err);
-			res.send(500);
-		});
+  let id = req.params.userId;
+  usersDb
+    .getUserById(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(500);
+    });
+});
+router.put(`/change-Profile-Pic`, (req, res) => {
+  const { userId, newPicLink } = req.body;
+  usersDb
+    .changeProfilePic(newPicLink, userId)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(500);
+    });
 });
 
 module.exports = router;
