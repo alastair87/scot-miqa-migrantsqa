c98f16b245c46c781939db7d10dbc71e5f193a64
Youssef910 <youssef.sadek910@gmail.com>
2019-10-16 22:07:15 +0100

    Create Sort type Menu

---
+++ client/src/components/SortTypeMenu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Dropdown, Icon } from "semantic-ui-react";
+
+export default props => {
+  return (
+    <Dropdown
+      item
+      size="small"
+      inline
+      text="Sort Questions "
+      icon="sort"
+      labeled
+      button
+      className="icon"
+    >
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={e => props.sortType(e, "score")}>
+          <Icon name="sort amount down" color="blue" />
+          <Icon name="thumbs up" color="blue" />
+          Likes
+        </Dropdown.Item>
+        <Dropdown.Item onClick={e => props.sortType(e, "date_posted")}>
+          <Icon name="sort numeric down" color="blue" />
+          <Icon name="calendar outline" color="blue" />
+          Date Posted
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
